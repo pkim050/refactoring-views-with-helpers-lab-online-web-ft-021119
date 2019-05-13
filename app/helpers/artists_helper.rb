9363a48c97808fc7a8af3d2afa84a737d38a452b
Patrick Kim <pkim050@gmail.com>
2019-05-13 12:16:06 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,6 +1,6 @@
 module ArtistsHelper
   def display_artist(song)
-    link_to song.artist.name, song.artist and return if song.artist
-    link_to "Add Artist", song
+    link_to song.artist.name, artist_path(song.artist) and return if song.artist
+    link_to "Add Artist", edit_song_path(song)
   end
 end
